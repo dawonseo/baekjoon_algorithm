155c60edc930f0b38997a7779f1b44172ee51ab6
dawonseo <oloielo@naver.com>
2022-03-03 16:34:49 +0900

    Creat 이진수 변환

---
+++ 10829_이진수_변환.py
@@ -0,0 +1,16 @@
+# 백준 10829 이진수 변환
+
+# 재귀함수 사용하지 않음
+N = int(input())
+print(bin(N)[2:])
+
+# 재귀함수 사용
+def to_bin(num):
+    if num == 0:
+        return ''
+    elif num == 1:
+        return '1'
+    return to_bin(num//2) + str(num%2)
+
+N = int(input())
+print(to_bin(N))
