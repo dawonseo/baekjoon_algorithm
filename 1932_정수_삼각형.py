28315ec5a5e83e918a6099bcdb9276c7da4d50c8
Dawon Seo <85838334+dawonseo@users.noreply.github.com>
2022-08-15 17:23:29 +0900

    Add 1932_정수_삼각형

---
+++ 1932_정수_삼각형.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+
+def solution(triangle):
+    for i in range(1, len(triangle)):
+        for j in range(len(triangle[i])):
+            if j == 0:                                      # 왼쪽 끝일 경우
+                triangle[i][j] += triangle[i - 1][j]
+            elif j == len(triangle[i]) - 1:                 # 오른쪽 끝일 경우
+                triangle[i][j] += triangle[i - 1][j - 1]
+            else:                                           # 중간일 경우
+                triangle[i][j] += max(triangle[i - 1][j - 1], triangle[i - 1][j])
+
+    return max(triangle[-1])
+
+
+N = int(input())
+triangle = []
+
+for _ in range(N):
+    triangle.append(list(map(int, input().split())))
+
+print(solution(triangle))
