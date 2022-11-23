144f240911383e3e9af0222b6b2469380c539479
Dawon Seo <85838334+dawonseo@users.noreply.github.com>
2022-11-23 14:52:59 +0900

    Add 15649_N과_M_1

---
+++ 15649_N과_M_1.py
@@ -0,0 +1,13 @@
+N, M = map(int, input().split())
+
+
+def solve(arr):
+    if len(arr) == M:
+        print(" ".join(map(str, arr)))
+    else:
+        for i in range(1, N+1):
+            if i not in arr:
+                solve(arr + [i])
+
+
+solve([])
